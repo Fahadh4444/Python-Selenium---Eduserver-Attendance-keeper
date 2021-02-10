9fd36468942c5b2a917a2b8f13543f62717430b3
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2021-02-11 01:28:33 +0530

    Python-Selenium---Eduserver-Attendance-keeper
    This helps you atleast relax in between classes without hurriedly marking attendance😎😎😎

---
+++ Keeper.py
@@ -0,0 +1,62 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+PATH = "F:\Softwares & Tools\Chrome Webdriver\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+
+url = 'https://eduserver.nitc.ac.in/login/index.php'
+
+driver.get(url)
+
+
+driver.find_element_by_xpath('//*[@id="username"]').send_keys('username')
+driver.find_element_by_xpath(
+    '//*[@id="password"]').send_keys('password')
+driver.find_element_by_xpath(
+    '/html/body/div[1]/div[2]/div/section/div[2]/div[2]/div[1]/div/div[2]/form/div[3]/button').click()
+
+a = 1
+b = 0
+list = driver.find_elements_by_link_text('Submit attendance')
+if(list != []):
+    b = 1
+
+driver.implicitly_wait(10)
+time.sleep(3)
+driver.find_element_by_partial_link_text('Attendance').click()
+time.sleep(2)
+driver.implicitly_wait(5)
+driver.find_element_by_link_text('Go to activity').click()
+driver.implicitly_wait(3)
+
+
+while(a):
+    # driver.implicitly_wait(10)
+    # time.sleep(3)
+    # driver.find_element_by_partial_link_text('Attendance').click()
+    # time.sleep(2)
+    # driver.implicitly_wait(5)
+    # driver.find_element_by_link_text('Go to activity').click()
+    # driver.implicitly_wait(3)
+
+    if(b):
+        time.sleep(2)
+        driver.find_element_by_link_text('Submit attendance').click()
+        time.sleep(2)
+        driver.implicitly_wait(2)
+        driver.find_element_by_xpath('//*[@id="id_status_3318"]').click()
+        driver.find_element_by_xpath('//*[@id="id_submitbutton"]').click()
+        a = 0
+    else:
+        driver.implicitly_wait(1)
+        time.sleep(5)
+        driver.refresh()
+        # driver.find_element_by_link_text('Home').click()
+
+driver.close()
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Python-Selenium---Eduserver-Attendance-keeper
+
+Hi guys,  
+This is Fahadh,  
+Yeah I know now a days its becaming hard to mark attendance in eduserver, due to our neglegence or some other at home it misses sometimes in just seconds.  
+To solve this issuse, this repo small code written in python using it's Selenium package.
+
+### After downloading this repo all have to do some steps in order to run it properly
+
+- Install selenium package in our pc
+  - using command  
+    <code><b>pip install selenium</b></code>
+- Then you have to download web driver for chrome  
+  Follow this link to [download it.](https://sites.google.com/a/chromium.org/chromedriver/)  
+  `NOTE: Download Chrome webdriver which supports your PC's Chrome brower.`
+- After this unzip downloaded file, copy chromewebdriver.exe, paste some where in PC except in C drive and copy it's path(address).
+- Now open our repo in code editor to make some changes in code
+  ![alt text](requiredChanges.PNG)
+  - Change path to path where you pasted web driver and add "\chromedriver.exe" at the end if required.
+  - Fill your username in place of username as shown in photo below.
+  - Do it same for your password too.
+
+### Now your code is ready to run, Open terminal or GIT Bash in repo and run [Kepper.py](Keeper.py) file.
+
+This helps you atleast relax in between classes without hurriedly marking attendance😎😎😎
