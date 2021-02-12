e148a22ea820b9149b1d39f173129806b9489298
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2021-02-12 11:43:11 +0530

    Updated Code

--- Keeper.py
+++ Keeper.py
@@ -21,7 +21,6 @@ driver.find_element_by_xpath(
 driver.find_element_by_xpath(
     '/html/body/div[1]/div[2]/div/section/div[2]/div[2]/div[1]/div/div[2]/form/div[3]/button').click()
 
-
 driver.implicitly_wait(10)
 time.sleep(3)
 driver.find_element_by_partial_link_text('Attendance').click()
@@ -29,7 +28,6 @@ time.sleep(2)
 driver.implicitly_wait(5)
 driver.find_element_by_link_text('Go to activity').click()
 driver.implicitly_wait(3)
-
 a = 1
 b = 0
 list = driver.find_elements_by_link_text('Submit attendance')
@@ -49,7 +47,14 @@ while(a):
     else:
         driver.implicitly_wait(1)
         time.sleep(5)
-        driver.refresh()
+        driver.find_element_by_link_text('Home').click()
+        driver.implicitly_wait(10)
+        time.sleep(3)
+        driver.find_element_by_partial_link_text('Attendance').click()
+        time.sleep(2)
+        driver.implicitly_wait(5)
+        driver.find_element_by_link_text('Go to activity').click()
+        driver.implicitly_wait(3)
         list = driver.find_elements_by_link_text('Submit attendance')
         if(list != []):
             b = 1
